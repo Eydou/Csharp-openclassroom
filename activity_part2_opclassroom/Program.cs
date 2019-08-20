3fcbef85a5368e4f570d210c88e7d39ad655fb15
TEST <edoaurd.touch@epitech.eu>
2019-08-20 19:50:36 +0100

    [ADD] activity part 2

---
+++ activity_part2_opclassroom/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Activity1_opclassroom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rndm = new Random();
+            int[] array = new int[7];
+
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rndm.Next(1, 49);
+            foreach (int nb in array)
+                Console.WriteLine(nb);
+        }
+    }
+}
